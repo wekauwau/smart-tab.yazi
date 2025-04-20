bf863cb9d5c523c853aebd7b2c0bee7efba14393
wekauwau <wekauwau@gmail.com>
2025-04-20 16:52:42 +0700

    chore: update from wiki

--- main.lua
+++ main.lua
@@ -2,6 +2,6 @@
 return {
 	entry = function()
 		local h = cx.active.current.hovered
-		ya.manager_emit("tab_create", h and h.cha.is_dir and { h.url } or { current = true })
+		ya.mgr_emit("tab_create", h and h.cha.is_dir and { h.url } or { current = true })
 	end,
 }
